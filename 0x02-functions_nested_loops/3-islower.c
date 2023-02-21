1331f9bb888f14cee48bb4b26af568e406854ebb
grufics <a.grufics@gmail.com>
2023-02-21 04:49:46 -0800

    task3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islowerr - the funtion to compare
+ * @c: contains parameter to be compared
+ * Return: 1 if lowercase and 0 if otherwise
+ */
+int _islower(int c)
+
+{
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
