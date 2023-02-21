39afea1632c13577b6ebe3947840e4dc0c8e27da
grufics <a.grufics@gmail.com>
2023-02-21 09:22:06 -0800

    Task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -30,6 +30,16 @@ void print_to_98(int n)
 		}
 		printf("\n");
 	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
 	else if (n == 98)
 		printf("%d", n);
 }
