f27f9cc31bca0f934fa26f39c6482b060549ea5b
grufics <a.grufics@gmail.com>
2023-02-23 05:33:25 -0800

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0-9
+ * Description: prints 0-9 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 15; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+			}
+			_putchar('0' + (i % 10));
+		}
+		_putchar('\n');
+	}
+}
