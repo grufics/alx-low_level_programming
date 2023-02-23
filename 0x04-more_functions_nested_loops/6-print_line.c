1c96bebcb6194821c15d15e9e011fe0d17ea64a0
grufics <a.grufics@gmail.com>
2023-02-23 05:34:46 -0800

    task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * Description: prints a line of length n
+ * @n: determines how long the line would be
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
