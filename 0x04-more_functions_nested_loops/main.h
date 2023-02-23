b4b779270f946c36f157191d16493a239df7e476
grufics <a.grufics@gmail.com>
2023-02-23 05:01:27 -0800

    task0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,33 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+int _putchar(char c);
+
+void print_numbers(void);
+
+void print_most_numbers(void);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+void print_line(int n);
+
+void more_numbers(void);
+
+void print_diagonal(int n);
+
+int mul(int, int);
+
+void print_square(int n);
+
+void print_triangle(int n);
+
+void print_number(int n);
+
+#endif
