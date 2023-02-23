29952cd36c647e5eace7a1c570bfacb912e8a21f
grufics <a.grufics@gmail.com>
2023-02-23 05:05:19 -0800

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * Description: returns the product of x and y
+ * @x: parameter of type int
+ * @y: parameter of type int
+ * Return: x * y
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
